8966dd2c1190c699d6a7e90887fedd7a1bcfe465
operlman <operlman@mgh.harvard.edu>
2023-08-07 14:59:05 +0300

    ch2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { PublicationsComponent } from './publications/publications.component';
-import { TeamComponent } from './team/team.component';
-import { CareersComponent } from './careers/careers.component';
 import { ContactComponent } from './contact/contact.component';
 import { SoftwareComponent } from './software/software.component';
-import { FundingComponent } from './funding/funding.component';
 import { ResearchComponent } from './research/research.component';
 
 @NgModule({
@@ -20,10 +17,7 @@ import { ResearchComponent } from './research/research.component';
     HomeComponent,
     BsNavbarComponent,
     PublicationsComponent,
-    TeamComponent,
-    CareersComponent,
     ContactComponent,
-    FundingComponent,
     SoftwareComponent,
     ResearchComponent
   ],
